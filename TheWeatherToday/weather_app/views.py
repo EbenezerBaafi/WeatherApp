714a7fff87af34c6f42a00fff9efd9b82aa3688a
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-09-26 15:53:55 +0000

    feat: add weather view to fetch and display city weather data from OpenWeatherMap API

--- TheWeatherToday/weather_app/views.py
+++ TheWeatherToday/weather_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import json
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, welcome to The Weather Today!")
+def weather(request):
+    if request.method == 'POST':
+        # Here you would typically handle the POST request data
+        city = request.POST['city']
+        source = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=impereial&appid=9eeb671c6507280051b8eecd51c8abff'
+        list_of_data = requests.get(source.format(city)).json()
+
+        data = {
+            'country_code': str(list_of_data['sys']['country']),
+            'coordinate': str(list_of_data['coord']['lon']) + ' ' + str(list_of_data['coord']['lat']),
+            'temp': round((list_of_data['main']['temp'] - 32) * 5/9, 2),
+            'humidity': str(list_of_data['main']['humidity']),
+            'pressure': str(list_of_data['main']['pressure']),
+        }
+    else:
+        data = {}
+        return render(request, 'weather_app.html', data)
